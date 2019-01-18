97daa37ab9e1b54ae90e314f1ef2c05a37c5697d
shshwdr <cs.vivian04@gmail.com>
2019-01-18 00:35:33 -0800

    add fruit spawner

--- Assets/Fruit.cs
+++ Assets/Fruit.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     public GameObject fruitSlicedPrefab;
+    Rigidbody2D rb;
+    public float startForce = 15f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up*startForce,ForceMode2D.Impulse);
+    }
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,8 +19,9 @@ public class Fruit : MonoBehaviour
         {
             Vector3 direction = (collision.transform.position - transform.position).normalized;
             Quaternion rotation = Quaternion.LookRotation(direction);
-            Instantiate(fruitSlicedPrefab,transform.position,rotation);
+            GameObject go =  Instantiate(fruitSlicedPrefab,transform.position,rotation);
             Destroy(gameObject);
+            Destroy(go, 5);
         }
     }
 }
---
+++ Assets/FruitSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject fruit;
+    public Transform[] spawnPoints;
+    public float minDelay = 0.1f;
+    public float maxDelay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+    IEnumerator SpawnFruits()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+            int spawnPositionIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPosition = spawnPoints[spawnPositionIndex];
+            GameObject go = Instantiate(fruit, spawnPosition.position, spawnPosition.rotation);
+            Destroy(go, 5);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
