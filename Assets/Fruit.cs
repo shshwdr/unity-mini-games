d66216cfbd75a6e839ecc8ba0e84ed52c0b8cd60
shshwdr <cs.vivian04@gmail.com>
2019-01-18 00:06:58 -0800

    slice fruit into two

---
+++ Assets/Fruit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject fruitSlicedPrefab;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Blade")
+        {
+            Instantiate(fruitSlicedPrefab,transform.position,transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
